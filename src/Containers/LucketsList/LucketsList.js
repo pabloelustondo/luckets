fa6bce77af6733067c577f6c4386d328599692c7
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-01-14 07:49:54 -0500

    getting initial css boxes right

---
+++ src/Containers/LucketsList/LucketsList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./LucketsList.css";
+
+class LucketsList extends Component {
+  render() {
+    return <div className='LucketsList'></div>;
+  }
+}
+
+export default LucketsList;
