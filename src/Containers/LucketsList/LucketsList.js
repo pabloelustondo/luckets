ce785a57e6517e9be78b99ff206d9e192885a33f
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-01-14 22:00:05 -0500

    fixed the scroll for the inside div

--- src/Containers/LucketsList/LucketsList.js
+++ src/Containers/LucketsList/LucketsList.js
@@ -1,9 +1,52 @@
-import React, { Component } from "react";
-import "./LucketsList.css";
+import React, { Component } from 'react';
+import './LucketsList.css';
+
+import LucketsItem from  '../../FunctionalComponents/LucketItem/LucketItem';
 
 class LucketsList extends Component {
   render() {
-    return <div className='LucketsList'></div>;
+    return (<div className='LucketsList'>
+
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+<LucketsItem />
+
+
+
+    </div>);
   }
 }
 
---
+++ src/FunctionalComponents/LucketItem/LucketItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LucketItem = () => (
+    <div className='LucketItem'>
+        Lucket
+    </div>
+)
+
+export default LucketItem
