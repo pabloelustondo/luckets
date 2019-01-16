b4c676c8a56cea7496774e2970d0a0130684e469
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-01-15 23:32:04 -0500

    Reading luckets from database!

--- src/Containers/LucketsList/LucketsList.js
+++ src/Containers/LucketsList/LucketsList.js
@@ -1,51 +1,34 @@
 import React, { Component } from 'react';
 import './LucketsList.css';
 
+import axios from 'axios'
+
 import LucketsItem from  '../../FunctionalComponents/LucketItem/LucketItem';
 
 class LucketsList extends Component {
+
+  state = {luckets:{}};
+
+  componentDidMount = () => {
+    axios.get("https://luckets-5fbb4.firebaseio.com/luckets.json").then( 
+      response => 
+    {
+        this.setState({  luckets:response.data });
+    }).catch(
+        err => {
+            this.setState({
+                error: err
+            })
+            alert(err)}
+    )
+
+}
+
+
   render() {
-    return (<div className='LucketsList'>
-
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-<LucketsItem />
-
-
-
+    return (
+    <div className='LucketsList'>
+    { Object.keys(this.state.luckets).map(  key => <LucketsItem key={key} id={key}></LucketsItem>  ) }
     </div>);
   }
 }
--- src/FunctionalComponents/LucketItem/LucketItem.js
+++ src/FunctionalComponents/LucketItem/LucketItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const LucketItem = () => (
+const LucketItem = (props) => (
     <div className='LucketItem'>
-        Lucket
+        {props.id}
     </div>
 )
 
