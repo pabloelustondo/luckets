ca3047b3376eb5da3994d772793a17ba4a3c0193
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-01-16 22:46:38 -0500

    added material fonts

--- src/FunctionalComponents/LucketItem/LucketItem.js
+++ src/FunctionalComponents/LucketItem/LucketItem.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './LucketItem.css'
 
 const LucketItem = (props) => (
     <div className='LucketItem'>
+    <i class="material-icons">face</i>
         {props.id}
+    <i class="material-icons orange600" >face</i>
     </div>
 )
 
