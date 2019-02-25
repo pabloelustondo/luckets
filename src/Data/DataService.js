ed59e47594fca4be0099552ec5f90cd1ee98ce23
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-02-24 20:46:58 -0500

    name is almost there

--- src/Data/DataService.js
+++ src/Data/DataService.js
@@ -5,7 +5,11 @@ export const getData = handler => {
   axios
     .get("https://luckets-5fbb4.firebaseio.com/luckets.json")
     .then(response => {
-      let luckets = Object.keys(response.data).map(key => response.data[key]);
+      let luckets = Object.keys(response.data).map(key => 
+        { let obj = response.data[key];
+          obj.id = key;
+          return obj }
+        );
       let focusLucket = getRootLucket(luckets);
       handler(luckets, focusLucket);
     })
