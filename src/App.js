c0c83e0e11c39e44ba8604d18cbdbbdfe39bc73a
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-02-25 21:28:28 -0500

    added autehntication with google

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import LucketsList from './Containers/LucketsList'
-import Footer from './Funcs/Footer';
+import LucketsList from "./Containers/LucketsList";
+import firebase from "firebase";
+import { StyledFirebaseAuth } from "react-firebaseui";
+
+firebase.initializeApp({
+  apiKey : "AIzaSyAu-HXBAYHQOxksCHplaz9JbbvJdrgVOGY",
+  authDomain: "luckets-5fbb4.firebaseapp.com"
+});
 
 class App extends Component {
+  state = { signedIn: false ,
+            user: null};
+
+
+  signOut = () => {
+    debugger;
+    this.setState({ signedIn: false, user: null });
+    firebase.auth().signOut().then(function() {
+      alert("Signed out")
+    }).catch(function(error) {
+      alert("Signing out ERROR "+ error);
+    });
+
+  }          
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      debugger;
+      if (user !== null){
+        this.setState({ signedIn: user.I, user: user });
+      } else {
+        this.setState({ signedIn: false, user: null });
+      }
+
+    });
+
+  };
+
   render() {
     return (
       <div className="App">
-        <LucketsList />
+        {this.state.signedIn ? (
+          <LucketsList user={this.state.user} signOut={this.signOut}/>
+        ) : (<StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />)}
       </div>
     );
   }
--- src/Funcs/Header/index.js
+++ src/Funcs/Header/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import './index.css'
-import AddLucketIcon from '../AddLucketIcon'
+import React from "react";
+import "./index.css";
+import AddLucketIcon from "../AddLucketIcon";
+//signOut={this.singOut}
+const Header = props => (
+  <div className="Header" onClick={props.addLucket}>
+    {props.user ? props.user.displayName : null}
+    <button onClick={props.signOut}> SignOut </button>
+    <AddLucketIcon addLucket={props.addLucket} />
+  </div>
+);
 
-const Header = (props) => (<div className='Header' onClick = {props.addLucket} >
-    <AddLucketIcon  addLucket={props.addLucket}/>
-</div>)
-
-export default Header
+export default Header;
