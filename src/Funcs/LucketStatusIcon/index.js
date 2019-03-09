81ce409bdab67f7a7e265b3a8a0ae8ee860ecec5
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-03-09 00:51:12 -0500

    now with a new edit visual mecanism for status

--- src/Funcs/LucketStatusIcon/index.js
+++ src/Funcs/LucketStatusIcon/index.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './index.css'
-import { red } from 'ansi-colors';
 
+const colorClass = (color) => "LucketStatusIcon status-"+ color;
+class LucketStatusIcon extends Component {
 
-const colorClass = (color) => 'status-' + color;
-const LucketStatusIcon = props => {
-    ;
-    return (<div className={'LucketStatusIcon ' + colorClass(props.status)} style={{ backgroundColor: props.status }} onClick={() => { }} ></div>)
+state = {
+    edit : false
+}
+
+edit = () => {
+    debugger;
+    if (this.state.edit === false){
+        this.setState({edit:true})
+    } else {
+        this.setState({edit:false}) 
+    }
+
+}
+
+set = (event, v) => {
+    debugger;
+    event.stopPropagation();
+    this.setState({edit:false}) 
 }
     
+render() {
 
+    let options = ['blue','green','white','yellow','red','purple','black'];
+
+    return (<div className={'LucketStatusIcon ' + colorClass(this.props.status)} 
+    style={{ backgroundColor: this.props.status }} 
+    onClick={this.edit}
+     >
+    { (this.state.edit === true)?
+         <div id="menu" className="Menu">
+         {options.filter(
+             o => o!==this.props.status
+         ).map( o => 
+            <div className={'LucketStatusIcon ' + colorClass(o)} 
+            onClick={(e) => this.set(e,o)} />
+         )}
+         </div>:null }
+    </div>)
+}
+    
+}
 
 export default LucketStatusIcon
