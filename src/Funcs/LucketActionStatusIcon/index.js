3a57585249ec54627b500661ca09881bac888986
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-03-10 15:40:18 -0400

    change of action status

--- src/Funcs/LucketActionStatusIcon/index.js
+++ src/Funcs/LucketActionStatusIcon/index.js
@@ -23,15 +23,27 @@ class LucketActionStatusIcon extends Component {
     
     set = (event, v) => {
         event.stopPropagation();
-        this.setState({edit:false}) 
+        this.setState({edit:false});
+        let lucket = this.props.lucket;
+        lucket.actionStatus = v;
+        this.props.updateLucket(lucket);
     }
 
     setPoints = (event, v) => {
         event.stopPropagation();
         if (v==="plus"){
             let lucket = this.props.lucket;
+            if (lucket.points < 25){
             lucket.points += 1;
             this.props.updateLucket(lucket)
+            }
+        }
+        if (v==="minus"){
+            let lucket = this.props.lucket;
+            if (lucket.points > 1){
+                lucket.points -= 1;
+                this.props.updateLucket(lucket)
+            }
         }
     }
 
