b54d30405fadee823e662d3196c24cd8e2fbb1a6
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-03-10 19:04:21 -0400

    fixed refresh button

--- src/Funcs/SettingsIcon/index.js
+++ src/Funcs/SettingsIcon/index.js
@@ -26,7 +26,7 @@ class SettingsIcon extends Component {
       <div className="SettingsPanel">
       {this.props.user ? <div>{this.props.user.displayName}</div> : null}
       <button onClick={this.props.signOut}> SignOut </button>
-      <button onClick={this.props.signOut}> Refresh </button>
+      <button onClick={this.props.refresh}> Refresh </button>
       </div>:null}
       </div>
     );
