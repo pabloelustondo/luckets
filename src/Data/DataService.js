2d3811bb3b5a147b861030c8aea2a062b5135c46
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-03-12 19:54:00 -0400

    removed alert

--- src/Data/DataService.js
+++ src/Data/DataService.js
@@ -59,7 +59,6 @@ export const postData = (user, lucket, handler) => {
   axios
     .patch("https://luckets-5fbb4.firebaseio.com//users/"+user.uid+"/luckets/" + lucket.id +".json", lucket)
     .then(response => {
-      alert("POsted ok")
     })
     .catch(err => {
       alert("ERROR" + err);
