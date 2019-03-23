207ce3fa406646923717debd2420e470a5bc69a2
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-03-23 17:36:04 -0400

    can change category and order

--- src/Funcs/LucketCategory/index.js
+++ src/Funcs/LucketCategory/index.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
 import './index.css'
-
-
-const colorClass = (color) => 'status-'+color;
 const colorFloatClass = (color) => "LucketCategoryIcon statusfloat-"+ color;
 
 class LucketCategory extends Component {
@@ -25,7 +22,7 @@ class LucketCategory extends Component {
         event.stopPropagation();
         this.setState({edit:false});
         let lucket = this.props.lucket;
-        lucket.actionStatus = v;
+        lucket.category = v;
         this.props.updateLucket(lucket);
     }
 
@@ -33,54 +30,65 @@ class LucketCategory extends Component {
         event.stopPropagation();
         if (v==="plus"){
             let lucket = this.props.lucket;
-            if (lucket.points < 25){
-            lucket.points += 1;
-            this.props.updateLucket(lucket)
+            if (lucket.order && lucket.order < 25){
+            lucket.order += 1;
             }
+            if (!lucket.order){lucket.order=1}
+            this.props.updateLucket(lucket)
         }
         if (v==="minus"){
             let lucket = this.props.lucket;
-            if (lucket.points > 1){
-                lucket.points -= 1;
-                this.props.updateLucket(lucket)
+            if (lucket.order && lucket.order > 1){
+                lucket.order -= 1;
             }
+            if (!lucket.order){lucket.order=1}
+            this.props.updateLucket(lucket)
         }
     }
 
     render() {    
 
 
-    let options = ['blue','green','white','yellow','red','purple','black'];
+    let options = ['A','B','C','D','E','F','G','H'];
 
+
+    const categoryEditor =  (this.state.edit === true)?
+        <div>
+        <div className="CategoryCallout"/>
+        <div id="menu" className="CategoryMenu">
+        {options.filter(
+            o => o!==this.props.status
+        ).map( o => 
+           <div key={o} className={'LucketCategoryIcon'} 
+           onClick={(e) => this.set(e,o)} >{o}</div>
+        )}
+
+        <div key={"minus"} className={'LucketCategoryIcon ' + colorFloatClass("minus")} 
+           onClick={(e) => this.setPoints(e,"minus")} >
+           <img src="/images/minus.svg" className="CategoryImage" alt="minus.svg" />
+           </div>
+
+       <div key={"plus"} className={'LucketCategoryIcon ' + colorFloatClass("plus")} 
+           onClick={(e) => this.setPoints(e,"plus")} >
+           <img src="/images/add.svg" className="CategoryImage" alt="add.svg" />
+           </div>
+
+        <div key={"close"} className={'LucketCategoryIcon ' + colorFloatClass("plus")} 
+           onClick={this.edit} >
+           <img src="/images/delete.svg" className="CategoryImage" alt="delete.svg" />
+           </div>
+
+        </div></div>:null
 
     return (
-    <div className={ 'LucketCategoryIcon ' + colorClass(this.props.status)} 
+    <div className={ 'LucketCategoryIcon'} 
     onClick={this.edit}>
-        {this.props.lucket.points}
-        
-        { (this.state.edit === true)?
-         <div>
-         <div className="CategoryCallout"/>
-         <div id="menu" className="CategoryMenu">
-         {options.filter(
-             o => o!==this.props.status
-         ).map( o => 
-            <div key={o} className={'LucketCategoryIcon ' + colorFloatClass(o)} 
-            onClick={(e) => this.set(e,o)} />
-         )}
 
-         <div key={"minus"} className={'LucketCategoryIcon ' + colorFloatClass("minus")} 
-            onClick={(e) => this.setPoints(e,"minus")} >
-            <img src="/images/minus.svg" className="CategoryImage" alt="minus.svg" />
-            </div>
+        {this.props.lucket.category}
 
-        <div key={"plus"} className={'LucketCategoryIcon ' + colorFloatClass("plus")} 
-            onClick={(e) => this.setPoints(e,"plus")} >
-            <img src="/images/add.svg" className="CategoryImage" alt="add.svg" />
-            </div>
+        {this.props.lucket.order}
 
-         </div></div>:null }
-
+        {categoryEditor}
 
     </div>
     )
