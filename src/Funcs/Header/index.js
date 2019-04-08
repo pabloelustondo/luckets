2e8a5cbe70aeca7d7f0352ec336f4885c05e47d9
Pablo Elustondo <pabloelustondo@Pablos-MacBook-Pro.local>
2019-04-07 20:51:01 -0400

    header has more css structure and initial step button

--- src/Funcs/Header/index.js
+++ src/Funcs/Header/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./index.css";
 import AddLucketIcon from "../AddLucketIcon";
 import SettingsIcon from "../SettingsIcon";
-import Path from "./Path"
+import LucketStepIcon from "../LucketStepIcon";
+import Path from "./Path";
 
 //signOut={this.singOut}
 const Header = props => (
   <div className="Header">
-    <Path luckets={props.luckets} focusLucket={props.focusLucket}/>
-    <AddLucketIcon addLucket={props.addLucket} />
+    <div className="HeaderItemLeft">
+      <LucketStepIcon status="green" />
+    </div>
+
+    <div className="HeaderItemCenter">
+      <Path luckets={props.luckets} focusLucket={props.focusLucket} />
+    </div>
+
+    <div className="HeaderItemRigth">
+      <AddLucketIcon addLucket={props.addLucket} />
+    </div>
   </div>
 );
 
---
+++ src/Funcs/LucketTimeFrameIcon/index.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import './index.css'
+
+const colorClass = (color) => "LucketStatusIcon status-"+ color;
+const colorFloatClass = (color) => "LucketStatusIcon statusfloat-"+ color;
+class LucketStatusIcon extends Component {
+
+state = {
+    edit : false
+}
+
+edit = () => {
+    if (this.state.edit === false){
+        this.setState({edit:true})
+    } else {
+        this.setState({edit:false}) 
+    }
+
+}
+
+set = (event, v) => {
+    event.stopPropagation();
+    this.setState({edit:false}) 
+    let lucket = this.props.lucket;
+    lucket.status = v;
+    this.props.updateLucket(lucket);
+}
+    
+render() {
+
+    let options = ['blue','green','white','yellow','red','purple','black'];
+
+    return (
+    <div className={'LucketStatusIcon ' + colorClass(this.props.status)} 
+    style={{ backgroundColor: this.props.status }} 
+    onClick={this.edit}> 
+    { (this.state.edit === true)?
+         <div>
+         <div className="Callout"/>
+         <div id="menu" className="Menu">
+         {options.filter(
+             o => o!==this.props.status
+         ).map( o => 
+            <div className={'LucketStatusIconShort ' + colorFloatClass(o)} 
+            onClick={(e) => this.set(e,o)} />
+         )}
+         </div></div>:null }
+    </div>)
+}
+    
+}
+
+export default LucketStatusIcon
